4a1018f2ceef6da05b24b91898f03ee1402020b0
JuliaTomchuk <yuliamalykowa@gmail.com>
2024-11-30 22:16:41 +0300

    fix tests

--- src/test/java/by/andersen/employee/TestConfig.java
+++ src/test/java/by/andersen/employee/TestConfig.java
@@ -2,6 +2,9 @@ package by.andersen.employee;
 
 import java.time.Instant;
 import java.util.Collections;
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -10,7 +13,11 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration
 @Testcontainers
-public class TestSecurityConfig {
+public class TestConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     public JwtDecoder jwtDecoder() {
         return token -> Jwt.withTokenValue(token)
@@ -20,6 +27,15 @@ public class TestSecurityConfig {
                 .issuedAt(Instant.now())
                 .expiresAt(Instant.now().plusSeconds(3600))
                 .build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/liquibase/changelog-master.yml");
+        liquibase.setContexts("!add-employees");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 
 }
